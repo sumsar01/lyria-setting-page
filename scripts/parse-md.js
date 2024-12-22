05d37ee515ebb4d2e3e26851a27234f58637a27f
Rasmus Skriver <skriver@live.dk>
2024-12-22 23:55:47 +0100

    dont use boomer script

--- scripts/parse-md.js
+++ scripts/parse-md.js
@@ -1,5 +1,5 @@
-const fs = require('fs');
-const path = require('path');
+import fs from 'fs';
+import path from 'path';
 
 // Function to read all Markdown files recursively from a folder
 function getMarkdownFiles(dir) {
